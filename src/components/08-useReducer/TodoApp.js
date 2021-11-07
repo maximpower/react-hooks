0e520542b615ab854e9d5d891e41e7269ce21035
maximpower <maxiimruso7@gmail.com>
2021-11-07 16:48:52 +0100

    import adecuado

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 
 import { todoReducer } from "./todoReducer";
 import { TodoList } from "./components/TodoList";
